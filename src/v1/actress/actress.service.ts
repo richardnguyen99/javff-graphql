5c948c1603efb379e680168cbd7578f8a0b2c70b
Richard Nguyen <richard@richardhnguyen.com>
2025-09-13 11:26:23 -0700

    feat: implement actree query options

--- src/v1/actress/actress.service.ts
+++ src/v1/actress/actress.service.ts
@@ -29,6 +29,24 @@ export class ActressService {
       qb.andWhere("actress.cup = :cup", { cup: options.cup });
     }
 
+    if (options?.bust !== undefined) {
+      qb.andWhere("actress.bust >= :bust", { bust: options.bust });
+    }
+
+    if (options?.waist !== undefined) {
+      qb.andWhere("actress.waist >= :waist", { waist: options.waist });
+    }
+
+    if (options?.hip !== undefined) {
+      qb.andWhere("actress.hip >= :hip", { hip: options.hip });
+    }
+
+    if (options?.year !== undefined) {
+      qb.andWhere("EXTRACT(YEAR FROM actress.birthday) <= :year", {
+        year: options.year,
+      });
+    }
+
     return qb.getMany();
   }
 
@@ -99,6 +117,20 @@ export class ActressService {
 
     if (options?.cup) {
       qb.andWhere("actress.cup = :cup", { cup: options.cup });
+    }
+    if (options?.bust !== undefined) {
+      qb.andWhere("actress.bust >= :bust", { bust: options.bust });
+    }
+    if (options?.waist !== undefined) {
+      qb.andWhere("actress.waist >= :waist", { waist: options.waist });
+    }
+    if (options?.hip !== undefined) {
+      qb.andWhere("actress.hip >= :hip", { hip: options.hip });
+    }
+    if (options?.year !== undefined) {
+      qb.andWhere("EXTRACT(YEAR FROM actress.birthday) <= :year", {
+        year: options.year,
+      });
     }
 
     const totalQb = qb.clone();
--- src/v1/actress/dto/actress-query-options.input.ts
+++ src/v1/actress/dto/actress-query-options.input.ts
@@ -1,9 +1,21 @@
-import { InputType, Field, Int } from "@nestjs/graphql";
+import { InputType, Field, Int, Float } from "@nestjs/graphql";
 
 @InputType()
 export class ActressQueryOptionsInput {
   @Field({ nullable: true })
   cup?: string;
+
+  @Field(() => Float, { nullable: true })
+  bust?: number;
+
+  @Field(() => Float, { nullable: true })
+  waist?: number;
+
+  @Field(() => Float, { nullable: true })
+  hip?: number;
+
+  @Field(() => Int, { nullable: true })
+  year?: number;
 
   @Field({ nullable: true })
   after?: string;
